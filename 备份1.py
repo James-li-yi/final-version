e6878f6b73dd2b77561fc563478dc04c4b30a8df
James-li-yi <lty2005@stanford.edu>
2025-07-10 14:29:44 +0800

    Update 备份1.py

--- 备份1.py
+++ 备份1.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-import matplotlib.pyplot as plt
+
 import numpy as np
 
 # 页面配置
